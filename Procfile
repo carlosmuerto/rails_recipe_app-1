a264d9d73f534945d161875054935692c5ffd40c
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-04-04 00:20:48 -0400

    update seed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: rake db:migrate && bin/rails server -b 0.0.0.0 -p $PORT -e production    
+web: rake db:migrate RAILS_ENV=production && rake db:seed RAILS_ENV=production && bin/rails server -b 0.0.0.0 -p $PORT -e production    
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,169 +6,173 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-users = User.create(
-  [
-    {
-      name: 'Ruth Bradley',
-      email: 'ruth_bradley@testmail.com',
-      password: 'ruth_bradley_pass'
-    },
-    {
-      name: 'Leticia Barrett',
-      email: 'leticia_barrett@testmail.com',
-      password: 'leticia_barrett_pass'
-    },
-    {
-      name: 'Kaiden Carlson',
-      email: 'kaiden_carlson@testmail.com',
-      password: 'kaiden_carlson_pass'
-    },
-    {
-      name: 'Aubree Rojas',
-      email: 'aubree_rojas@testmail.com',
-      password: 'aubree_rojas_pass'
-    },
-    {
-      name: 'Trevon Rich',
-      email: 'trevon_rich@testmail.com',
-      password: 'trevon_rich_pass'
-    }
-  ]
-)
-
-puts "#{users.count} users created"
-
-def create_recipe(recipe_name, user_in, public)
-  Recipe.create(
-    name: "#{recipe_name} recipe",
-    preparation_time_seconds: 10.minute.second,
-    cooking_time_seconds: 5.minute.second,
-    description: "my only and unique #{recipe_name} recipe",
-    public:,
-    user: user_in
+if User.find_by_name("Ruth Bradley").present?
+  puts "already seeded"
+else
+  users = User.create(
+    [
+      {
+        name: 'Ruth Bradley',
+        email: 'ruth_bradley@testmail.com',
+        password: 'ruth_bradley_pass'
+      },
+      {
+        name: 'Leticia Barrett',
+        email: 'leticia_barrett@testmail.com',
+        password: 'leticia_barrett_pass'
+      },
+      {
+        name: 'Kaiden Carlson',
+        email: 'kaiden_carlson@testmail.com',
+        password: 'kaiden_carlson_pass'
+      },
+      {
+        name: 'Aubree Rojas',
+        email: 'aubree_rojas@testmail.com',
+        password: 'aubree_rojas_pass'
+      },
+      {
+        name: 'Trevon Rich',
+        email: 'trevon_rich@testmail.com',
+        password: 'trevon_rich_pass'
+      }
+    ]
   )
-end
 
-def create_hot_dog_with_ingredients(foods, user_in)
-  completed_recipe = create_recipe('hot dog', user_in, user_in.id.even?)
-  incompleted_recipes = create_recipe('salad', user_in, user_in.id.odd?)
+  puts "#{users.count} users created"
 
-  puts '  inserting completed recipe ingredients'
-  RecipeFood.create(
-    amount: 1,
-    food: Food.find_by_name('sausage'),
-    recipe: completed_recipe
-  )
-  puts '    inserted sausage'
-
-  RecipeFood.create(
-    amount: 1,
-    food: Food.find_by_name('hot dog breat'),
-    recipe: completed_recipe
-  )
-  puts '    inserted hot dog breat'
-
-  foods.each do |food|
-    RecipeFood.create(
-      amount: 10 + (user_in.id % 5),
-      food:,
-      recipe: completed_recipe
+  def create_recipe(recipe_name, user_in, public)
+    Recipe.create(
+      name: "#{recipe_name} recipe",
+      preparation_time_seconds: 10.minute.second,
+      cooking_time_seconds: 5.minute.second,
+      description: "my only and unique #{recipe_name} recipe",
+      public:,
+      user: user_in
     )
-    puts "    inserted #{food.name}: #{food.id} "
   end
 
-  puts '  inserting incompleted recipe ingredients'
+  def create_hot_dog_with_ingredients(foods, user_in)
+    completed_recipe = create_recipe('hot dog', user_in, user_in.id.even?)
+    incompleted_recipes = create_recipe('salad', user_in, user_in.id.odd?)
 
-  cabbage = Food.find_by_name('Cabbage')
+    puts '  inserting completed recipe ingredients'
+    RecipeFood.create(
+      amount: 1,
+      food: Food.find_by_name('sausage'),
+      recipe: completed_recipe
+    )
+    puts '    inserted sausage'
 
-  RecipeFood.create(
-    amount: 5 + (user_in.id % 5),
-    food: cabbage,
-    recipe: incompleted_recipes
-  )
-  puts "    inserted #{cabbage.name}: #{cabbage.id} "
+    RecipeFood.create(
+      amount: 1,
+      food: Food.find_by_name('hot dog breat'),
+      recipe: completed_recipe
+    )
+    puts '    inserted hot dog breat'
 
-  onions = Food.find_by_name('Onion')
+    foods.each do |food|
+      RecipeFood.create(
+        amount: 10 + (user_in.id % 5),
+        food:,
+        recipe: completed_recipe
+      )
+      puts "    inserted #{food.name}: #{food.id} "
+    end
 
-  RecipeFood.create(
-    amount: 5 + (user_in.id % 5),
-    food: onions,
-    recipe: incompleted_recipes
-  )
+    puts '  inserting incompleted recipe ingredients'
 
-  puts "    inserted #{onions.name}: #{onions.id} "
+    cabbage = Food.find_by_name('Cabbage')
 
-  tomatoes = Food.create(
-    name: 'Tomatoes',
-    unit: 'piece',
-    price: '1.1',
-    quantity: 0,
-    user: user_in
-  )
+    RecipeFood.create(
+      amount: 5 + (user_in.id % 5),
+      food: cabbage,
+      recipe: incompleted_recipes
+    )
+    puts "    inserted #{cabbage.name}: #{cabbage.id} "
 
-  RecipeFood.create(
-    amount: 5 + (user_in.id % 5),
-    food: tomatoes,
-    recipe: incompleted_recipes
-  )
+    onions = Food.find_by_name('Onion')
 
-  puts "    inserted #{tomatoes.name}: #{tomatoes.id} "
-end
+    RecipeFood.create(
+      amount: 5 + (user_in.id % 5),
+      food: onions,
+      recipe: incompleted_recipes
+    )
 
-def user_create_foods_and_recipes(user_in)
-  puts "inserting recipes and food to #{user_in.name}"
+    puts "    inserted #{onions.name}: #{onions.id} "
 
-  # gram kilogram liter milliliter ml piece
-  foods = Food.create([
-                        {
-                          name: 'sausage',
-                          unit: 'piece',
-                          price: '3.1',
-                          quantity: 2,
-                          user: user_in
-                        },
-                        {
-                          name: 'Mayonnaise',
-                          unit: 'ml',
-                          price: '0.005',
-                          quantity: 500,
-                          user: user_in
-                        },
-                        {
-                          name: 'Onion',
-                          unit: 'g',
-                          price: '0.003',
-                          quantity: 30,
-                          user: user_in
-                        },
-                        {
-                          name: 'Ketchup',
-                          unit: 'ml',
-                          price: '0.005',
-                          quantity: 300,
-                          user: user_in
-                        },
-                        {
-                          name: 'Cabbage',
-                          unit: 'g',
-                          price: '0.004',
-                          quantity: 100,
-                          user: user_in
-                        },
-                        {
-                          name: 'hot dog breat',
-                          unit: 'piece',
-                          price: '6.6',
-                          quantity: 2,
-                          user: user_in
-                        }
-                      ])
+    tomatoes = Food.create(
+      name: 'Tomatoes',
+      unit: 'piece',
+      price: '1.1',
+      quantity: 0,
+      user: user_in
+    )
 
-  puts "  #{foods.count} foods create for #{user_in.name}"
+    RecipeFood.create(
+      amount: 5 + (user_in.id % 5),
+      food: tomatoes,
+      recipe: incompleted_recipes
+    )
 
-  create_hot_dog_with_ingredients(foods, user_in)
-end
+    puts "    inserted #{tomatoes.name}: #{tomatoes.id} "
+  end
 
-users.each do |user|
-  user_create_foods_and_recipes user
+  def user_create_foods_and_recipes(user_in)
+    puts "inserting recipes and food to #{user_in.name}"
+
+    # gram kilogram liter milliliter ml piece
+    foods = Food.create([
+                          {
+                            name: 'sausage',
+                            unit: 'piece',
+                            price: '3.1',
+                            quantity: 2,
+                            user: user_in
+                          },
+                          {
+                            name: 'Mayonnaise',
+                            unit: 'ml',
+                            price: '0.005',
+                            quantity: 500,
+                            user: user_in
+                          },
+                          {
+                            name: 'Onion',
+                            unit: 'g',
+                            price: '0.003',
+                            quantity: 30,
+                            user: user_in
+                          },
+                          {
+                            name: 'Ketchup',
+                            unit: 'ml',
+                            price: '0.005',
+                            quantity: 300,
+                            user: user_in
+                          },
+                          {
+                            name: 'Cabbage',
+                            unit: 'g',
+                            price: '0.004',
+                            quantity: 100,
+                            user: user_in
+                          },
+                          {
+                            name: 'hot dog breat',
+                            unit: 'piece',
+                            price: '6.6',
+                            quantity: 2,
+                            user: user_in
+                          }
+                        ])
+
+    puts "  #{foods.count} foods create for #{user_in.name}"
+
+    create_hot_dog_with_ingredients(foods, user_in)
+  end
+
+  users.each do |user|
+    user_create_foods_and_recipes user
+  end
 end
